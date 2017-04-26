042c1491041a7f98e75927942ec6c00ebe5fc3e3
tukacsr <tukacsr@gmail.com>
2017-04-26 13:04:35 +0200

    006_FloatingInput/ ShowingBookmarks

--- src/app/components/006_FloatingInput/006_FloatingInput.component.ts
+++ src/app/components/006_FloatingInput/006_FloatingInput.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './006_FloatingInput.component.html',
   styleUrls: [`./006_FloatingInput.component.scss`]
 })
 
 export class FloatingInputComponent implements OnInit {
   bg: any;
+  bookmarkInput: any;
+  bookmarksList: any;
+  bookmarkForm: any;
 
   constructor() { }
 
   ngOnInit() {
     this.bg = document.querySelector('.background');;
     const input = document.querySelector('input[type=text]');
-    const overlay = document.querySelector('.overlay');
+    this.bookmarksList = document.querySelector('.bookmarks-list');
+    this.bookmarkForm = document.querySelector('.bookmark-form');
+    this.bookmarkInput = document.querySelector('input[type=text]');
 
     input.addEventListener('focusin', this.showFloater);
     input.addEventListener('focusout', this.closeFloater);
+    this.bookmarkForm.addEventListener('submit', this.createBookmark);
   }
 
   showFloater = () => {
@@ -27,5 +34,19 @@ export class FloatingInputComponent implements OnInit {
     if (this.bg.classList.contains('show-floater')) {
       this.bg.classList.remove('show-floater');
     }
+  }
+
+  createBookmark = (e) => {
+    e.preventDefault();
+
+    const title = this.bookmarkInput.value;
+    const bookmark = document.createElement('a');
+    bookmark.className = 'bookmark';
+    bookmark.innerText = title;
+    bookmark.href = '#';
+    bookmark.target = '_blank';
+
+    this.bookmarksList.appendChild(bookmark);
+    this.bookmarkForm.reset();
   }
 }
